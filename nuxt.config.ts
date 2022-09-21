cf8411538f551f0773388479b19ebaa800d0c5f2
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-09-21 17:24:45 +0900

    disable nitro preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,7 @@ import {defineNuxtConfig} from "nuxt/config";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    nitro: {
-        preset: "vercel",
-    },
+    // nitro: { preset: "vercel", },
     css: ['~/assets/css/tailwind.css'],
     build: {
         postcss: {
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,9 @@
   <div>
     <!-- font-weight: bold; font-size: 1.875rem; -->
     <h1 class="font-bold text-3xl">Home</h1>
+    <p>
+      This is a 'pages/index'
+    </p>
   </div>
 </template>
 
